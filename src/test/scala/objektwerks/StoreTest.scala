dc4dee1391a165eecac92610b2230232760ed9e6
objektwerks <objektwerks@runbox.com>
2023-09-13 10:26:23 -0400

    edited store test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -6,18 +6,17 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class StoreTest extends AnyFunSuite with Matchers {
-  val conf = ConfigFactory.load("test.conf")
+  val config = ConfigFactory.load("test.conf")
+  val store = Store(config)
 
   test("store") {
-    val store = Store(conf)
-
     val todo = Todo(task = "wash car")
     val id = store.addTodo(todo)
-    println(s"*** Todo Add: $id")
+    println(s"*** Add Todo: $id")
     id shouldBe 1
 
     val todoWithId = todo.copy(id = id)
-    println(s"*** Todo Update: $todoWithId")
+    println(s"*** Update Todo: $todoWithId")
     val updatedTodo = todoWithId.copy(task = "wash and dry car")
     val updated = store.updateTodo(updatedTodo)
     updated shouldBe true
